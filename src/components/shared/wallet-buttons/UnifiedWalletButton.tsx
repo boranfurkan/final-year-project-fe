8b6995800d5efc94570e74feecf74b2c60a901d3
boranfurkan <furkanboran123@gmail.com>
2025-03-06 15:01:36 +0000

    feat: create wallet button display component

---
+++ src/components/shared/wallet-buttons/UnifiedWalletButton.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import React from 'react';
+
+import { EthereumWalletButton } from './EthereumWalletButton';
+import { SolanaWalletButton } from './SolanaWalletButton';
+import { SuiWalletButton } from './SuiWalletButton';
+import { useWallet } from '@/contexts/WalletContext';
+import { ChainType } from '@/types/chain';
+import ChainSelector from '../ChainSelector';
+
+interface WalletButtonProps {
+  className?: string;
+  fullWidth?: boolean;
+}
+
+export const UnifiedWalletButton: React.FC<WalletButtonProps> = ({
+  className = '',
+  fullWidth = false,
+}) => {
+  const { selectedChain } = useWallet();
+
+  // Compute wallet button classes
+  const walletButtonClass = fullWidth ? 'w-full' : '';
+
+  const renderWalletButton = (chain: ChainType) => {
+    switch (chain) {
+      case 'ethereum':
+        return <EthereumWalletButton className={walletButtonClass} />;
+      case 'solana':
+        return <SolanaWalletButton className={walletButtonClass} />;
+      case 'sui':
+        return <SuiWalletButton className={walletButtonClass} />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div
+      className={`flex gap-2 items-center max-md:flex-col ${className} ${
+        fullWidth ? 'w-full' : ''
+      }`}
+    >
+      <ChainSelector />
+      <div
+        className={`flex-grow  max-md:w-full ${
+          fullWidth ? 'w-full' : 'w-[160px]'
+        }`}
+      >
+        {renderWalletButton(selectedChain)}
+      </div>
+    </div>
+  );
+};
+
+export default UnifiedWalletButton;
