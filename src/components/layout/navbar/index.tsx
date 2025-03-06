f2bca231c713d0df4c39a962b1ded7b16a882d0d
boranfurkan <furkanboran123@gmail.com>
2025-03-06 15:02:49 +0000

    feat: complete layout

---
+++ src/components/layout/navbar/index.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import React, { useState } from 'react';
+import { usePathname } from 'next/navigation';
+
+import DesktopNavigation from './DesktopNavigation';
+import MobileMenu from './MobileMenu';
+import Logo from '@/assets/Logo';
+
+const Navbar: React.FC = () => {
+  const pathname = usePathname();
+  const [isSheetOpen, setIsSheetOpen] = useState(false);
+  const [isWalletConnected, setIsWalletConnected] = useState(false);
+
+  const handleConnectWallet = () => {
+    // Implement your wallet connection logic here
+    setIsWalletConnected(true);
+  };
+
+  const handleDisconnectWallet = () => {
+    // Implement your wallet disconnection logic here
+    setIsWalletConnected(false);
+  };
+
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur">
+      <div className="container flex h-16 items-center justify-between">
+        {/* Logo Component */}
+        <div className="flex-1 w-full">
+          <Logo />
+        </div>
+
+        {/* Desktop Navigation Components */}
+        <DesktopNavigation
+          activePath={pathname}
+          isWalletConnected={isWalletConnected}
+          onWalletConnect={handleConnectWallet}
+          onWalletDisconnect={handleDisconnectWallet}
+        />
+
+        {/* Mobile Menu Component */}
+        <MobileMenu
+          isOpen={isSheetOpen}
+          setIsOpen={setIsSheetOpen}
+          activePath={pathname}
+          isWalletConnected={isWalletConnected}
+          onWalletConnect={handleConnectWallet}
+          onWalletDisconnect={handleDisconnectWallet}
+        />
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
